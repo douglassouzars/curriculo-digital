e7c26db749ca406d6c80f1750dce39c40028ece9
douglassouzars <douglassouza15.ds@gmail.com>
2024-08-14 00:17:13 -0300

    atualizando vueconfigjs para gitpage

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'  // Use a URL base para GitHub Pages
+    : '/',
   devServer: {
     headers: {
       'Content-Type': 'text/html; charset=UTF-8',
