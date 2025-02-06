efaea8f8a285093f3bf68fba7515aa6dad344ee0
Douglas Souza <douglas.souza@confea.org.br>
2025-02-06 18:47:51 -0300

    Atualiza dependências e configurações do Vue

--- vue.config.js
+++ vue.config.js
@@ -12,19 +12,21 @@
     // Outras configura��es do devServer podem ser adicionadas aqui
  // },
 //});
-const { defineConfig } = require('@vue/cli-service');
+const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'  // Use a URL base para GitHub Pages
+    ? '/curriculo-digital/'  // Nome do repositório
     : '/',
   devServer: {
     headers: {
       'Content-Type': 'text/html; charset=UTF-8',
     },
-    // Forçar todas as requisições a usarem UTF-8
     compress: true,
-    open: true,  // Para abrir o navegador automaticamente
+    open: true,
   },
 });
+
+
+
